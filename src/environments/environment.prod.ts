516d3ce24b1f432370f0cf041ca997306c2768ed
astrowq <brandon@qwang.org>
2021-10-03 13:16:24 +0200

    add environment.prod.ts to git

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  ui: {
+    ssl: true,
+    host: 'dashboard.baryonics.org',
+    port: 443,
+    nameSpace: '/',
+  },
+  rest: {
+    ssl: true,
+    host: 'baryonics-prod.herokuapp.com',
+    port: 443,
+    nameSpace: '/',
+  },
+};
